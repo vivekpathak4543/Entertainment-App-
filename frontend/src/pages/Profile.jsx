d4573add955cb41ccb0ab742e3312fb12494ee2c
Vivek Pathak <vivekpathak8198@gmail.com>
2024-07-08 14:00:35 +0530

    "Implemented Profile page with user authentication

---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,61 @@
+// importing from installed packages 
+import React, { useContext, useEffect } from 'react'
+import { BiSolidUserCircle } from 'react-icons/bi';
+import { MdEmail } from 'react-icons/md';
+import axios from 'axios';
+
+// importing from custom files
+import MyContext from '../context/myContext'
+import baseUrl from '../utils/baseUrl';
+import Login from '../authComponents/Login'
+import Logout from '../authComponents/Logout'
+
+// profile page 
+function Profile() {
+    const myState = useContext(MyContext);
+
+
+    useEffect(() => {
+        if (myState.isAuthenticated) {
+            const fetchUser = async () => {
+                const api = await axios.get(`${baseUrl}/user/profile`, {
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    withCredentials: true,
+                });
+                // console.log(api.data.user);
+                myState.setUser(api.data.user)
+            }
+            fetchUser();
+        }
+    }, [myState.isAuthenticated,myState])
+
+
+
+    return (
+        <>
+            {
+                myState.isAuthenticated === true ? (
+                    <div className='p-4 mt-2'>
+                        <div className='flex flex-col gap-2 mb-4'>
+                            <div className="flex items-center gap-3 text-lg">
+                                <BiSolidUserCircle />
+                                <span>  {" "} {myState.user.name}</span>
+                            </div>
+                            <div className="flex items-center gap-3 text-lg">
+                                <MdEmail />
+                                <span>  {" "} {myState.user.email} </span>
+                            </div>
+                        </div>
+                        <Logout />
+                    </div>
+                ) : (
+                    <Login />
+                )
+            }
+        </>
+    )
+}
+
+export default Profile
