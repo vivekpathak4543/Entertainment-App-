3ff60f7463443739222954b66a5fba028edf5511
Vivek Pathak <vivekpathak8198@gmail.com>
2024-07-08 13:15:52 +0530

    "Implemented dynamic search and result display

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,72 @@
+
+import { useForm } from "react-hook-form";
+import { LuSearch } from "react-icons/lu";
+import { useLocation, useNavigate } from "react-router-dom";
+
+// SearchBar component
+const SearchBar = () => {
+
+    //  getting current location
+    const { pathname } = useLocation();
+    const navigate = useNavigate();
+
+    const { register, handleSubmit, reset } = useForm();
+
+    // why it is giving undefined, is it due to search bar outside the routes 
+    // const { mediaType, searchQuery } = useParams();
+    // console.log(mediaType)
+    // console.log(searchQuery)
+
+    // Function to handle form submission
+    const onSubmit = (data) => {
+
+        // Switch statement to handle different pathnames
+        switch (pathname) {
+            case "/":
+                navigate(`all/search/${encodeURIComponent(data.searchQuery)}`);
+                break;
+            case "/movie":
+                navigate(`movie/search/${encodeURIComponent(data.searchQuery)}`);
+                break;
+            case "/tv":
+                navigate(`tv/search/${encodeURIComponent(data.searchQuery)}`);
+                break;
+            default:
+                navigate(`/all/search/${encodeURIComponent(data.searchQuery)}`);
+        }
+        // Reset form after submission
+        // from where it is comming 
+        reset();
+    };
+
+    return (
+        // Search form container with styling
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="w-full h-14 bg-deepBlue z-50 flex gap-3 justify-center items-center font-light text-HeadingXS lg:justify-evenly lg:text-HeadingM px-2 lg:py-5 lg:gap-0 rounded-lg">
+            {/* Search icon */}
+            <LuSearch className="text-xl lg:text-3xl" />
+
+            {/* Search input */}
+            <input
+                type="search"
+                name="searchQuery"
+                id="search"
+                {...register("searchQuery")}
+                placeholder={
+                    // Placeholder text based on the current pathname
+                    pathname.includes("movie")
+                        ? "Search for movies..."
+                        : pathname.includes("tv")
+                            ? "Search for TV series..."
+                            : pathname.includes("bookmarks")
+                                ? "Search for bookmarked media..."
+                                : "Search for movies and TV series..."
+                }
+                className="w-[95%] h-fit bg-transparent caret-darkRed focus:outline-none border-b-2 border-transparent focus:border-waikawaGrey lg:w-[94%] placeholder-text-wrap"
+            />
+        </form>
+    );
+};
+
+export default SearchBar; 
---
+++ frontend/src/components/SearchResult.jsx
@@ -0,0 +1,55 @@
+// from installed packages 
+import React, { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+import axios from 'axios';
+
+// from custom files 
+import Loading from './cssComponents/Loading';
+import Media from '../components/mediaComponents/Media'
+
+// base url 
+import baseUrl from '../utils/baseUrl';
+
+// search result page 
+function SearchResult() {
+
+    const { mediaType, searchQuery } = useParams();
+    const [mediaData, setMediaData] = useState([]);
+
+    // fetching search result 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const { data } = await axios.get(`${baseUrl}/media/${mediaType}/search/${searchQuery}`);
+                setMediaData(data.data);
+            } catch (error) {
+                // console.error("Error fetching media data:", error);
+            }
+        }
+        fetchData();
+    }, [mediaType, searchQuery]);
+
+
+    // css style 
+    const containerStyle = "p-4 mt-2 flex flex-col gap-6";
+    const headingStyle = "text-2xl md:text-4xl font-bold";
+    const wrapperStyle = "grid gap-10 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
+
+
+    return (
+        <div className={containerStyle}>
+            <h1 className={headingStyle}> Result for {searchQuery} </h1>
+            {
+                mediaData && mediaData.length > 0 ? (
+                    <div className={wrapperStyle}>
+                        <Media mediaData={mediaData} />
+                    </div>
+
+                ) : (
+                    <Loading />
+                )}
+        </div>
+    )
+}
+
+export default SearchResult
