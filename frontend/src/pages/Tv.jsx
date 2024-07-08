5cffd44cb7c4f15ef3c01cc4a6c50ebd93acc8f0
Vivek Pathak <vivekpathak8198@gmail.com>
2024-07-08 13:58:11 +0530

    Implemented TV page with dynamic loading of MoreMedia component and pagination

---
+++ frontend/src/pages/Tv.jsx
@@ -0,0 +1,43 @@
+// importing from installed packages 
+import React, { useState } from "react";
+import MoreMedia from "../components/MoreMedia";
+
+
+// movie page
+const Tv = () => {
+
+    // initial page numbers 
+    const [pageNumbers, setPageNumbers] = useState([1, 2, 3, 4, 5]);
+
+    // Function to fetch next page data
+    const fetchNextPage = () => {
+        setPageNumbers(prevPageNumbers => [...prevPageNumbers, pageNumbers[pageNumbers.length - 1] + 1]);
+    };
+
+    // css style 
+    const containerStyle = "p-4 mt-2 flex flex-col gap-6";
+    const headingStyle = "text-2xl md:text-4xl font-bold";
+
+    // Render Movie media 
+    return (
+        <div className={containerStyle}>
+            <h1 className={headingStyle}>Explore Tv Shows </h1>
+            {
+                pageNumbers.map((currentPage, index) => (
+                    <MoreMedia
+                        key={index}
+                        currentPage={currentPage}
+                        mediaType={"tv"}
+                    />
+                ))
+            }
+            <button
+                onClick={fetchNextPage}
+                className="px-6 py-3 mb-10 bg-red-700 text-white font-semibold rounded-lg hover:bg-red-600 transition duration-300">
+                Load More
+            </button>
+        </div>
+    );
+};
+
+export default Tv;
