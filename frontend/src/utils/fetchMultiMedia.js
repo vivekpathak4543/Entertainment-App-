9f31c987bd707f647ddcad2454c680bb6381d6e7
Vivek Pathak <vivekpathak8198@gmail.com>
2024-07-08 14:05:33 +0530

    Added fetch function for multiple media types based on pagination and media type

---
+++ frontend/src/utils/fetchMultiMedia.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import baseUrl from "./baseUrl";
+
+// Fetch multiple media based on type, trending, movie, tv
+const fetchMultiMedia = async (page, mediaType) => {
+  const { data } = await axios.get(`${baseUrl}/media/${mediaType}/${page}`);
+  return data.data;
+};
+
+export default fetchMultiMedia;
