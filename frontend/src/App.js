a59c4513b23fe62b27c5c8c4b25795f05f1ec50c
Vivek Pathak <vivekpathak8198@gmail.com>
2024-07-08 14:09:03 +0530

    Refactored App component for enhanced routing and UI logic.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,85 @@
-import logo from "./logo.svg";
-import "./App.css";
+// importing from packages
+import React, { useContext } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+
+import MyContext from "./context/myContext";
+import Toast from "./components/cssComponents/Toast";
+
+// importing pages
 import Navbar from "./pages/Navbar";
-import MyContext from "./context/MyContext";
+import Bookmarks from "./pages/Bookmark";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Profile from "./pages/Profile";
+import Tv from "./pages/Tv";
+
+//importing components
+import Error404 from "./components/Error404";
+import SearchBar from "./components/SearchBar";
+import SearchResult from "./components/SearchResult";
+import Details from "./components/mediaDetails/Details";
+
+// importing auth components
+import Login from "./authComponents/Login";
+import Register from "./authComponents/Register";
 
 function App() {
+  const location = useLocation();
+  const { toast } = useContext(MyContext);
+
+  // hiding search bar
+  const searchBarHiddenRoutes = [
+    "/profile",
+    "/profile/login",
+    "/profile/register",
+    "/bookmarks",
+  ];
+  const isSearchBarHidden = searchBarHiddenRoutes.includes(location.pathname);
+
+  // css
+  const containerstyle =
+    "bg-leanBlue relative h-screen w-screen overflow-y-scroll flex flex-col overflow-x-hidden items-center justify-evenly text-white lg:flex-row scrollbar-corner-transparent scrollbar-thin  scrollbar-thumb-darkRed scrollbar-track-transparent overflow-scroll py-5";
+  const wrapperStyle = "w-11/12 h-5/6 z-40 lg:h-full";
+
   return (
-    <div className="App">
+    <div className={containerstyle}>
+      {/* Header will always be shown  */}
       <Navbar />
+      <div className={wrapperStyle}>
+        {/* conditionally  rendering of searchbar + */}
+        {!isSearchBarHidden && <SearchBar />}
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          {/* movies & tv routes  */}
+          <Route path="/movie" element={<Movie />} />
+          <Route path="/tv" element={<Tv />} />
+
+          {/* movie & tv details route */}
+          <Route path="/movie/:mediaId" element={<Details />} />
+          <Route path="/tv/:mediaId" element={<Details />} />
+
+          {/* bookmarks route  */}
+          <Route path="/bookmarks" element={<Bookmarks />} />
+
+          {/* profile routes  */}
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/login" element={<Login />} />
+          <Route path="/profile/register" element={<Register />} />
+
+          {/* search routes  */}
+          <Route
+            path="/:mediaType/search/:searchQuery"
+            element={<SearchResult />}
+          />
+
+          {/* error routes */}
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </div>
+
+      {toast && <Toast />}
     </div>
   );
 }
