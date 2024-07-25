a60506409873392b0a2a6f6ce9d27b25cc42124b
Vivek Pathak <76717203+vivekpathak4543@users.noreply.github.com>
2024-07-25 14:45:43 +0530

    Update myState

--- frontend/src/context/myState.js
+++ frontend/src/context/myState.js
@@ -5,7 +5,7 @@ import context from "./myContext";
 // where we have passed whole app code in authState props
 function MyState(props) {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
   const [toast, setToast] = useState(false);
   const [toastMessage, setToastMessage] = useState(null);
 
